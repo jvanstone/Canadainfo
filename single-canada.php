c38f9a2de96da60702aed097cbadc69f71979758
jvanstone <jason@vanstoneonline.com>
2021-02-18 10:56:18 -0500

    updated readme and screenshot

--- single-canada.php
+++ single-canada.php
@@ -1,7 +1,7 @@
 <?php 
 /**
- * Template Name: Guide Layout
- * Description: 
+ * Template Name: Canadian Guide Layout
+ * Description:  
  * Created by: Vanstone Online - Jason Vanstone
  *
  *
@@ -9,14 +9,19 @@
 ?>
 
 <?php get_header(); ?>
+
 <main id="content" class="guide_width">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php if ( comments_open() && ! post_password_required() ) { comments_template( '', true ); } ?>
-<?php endwhile; endif; ?>
-<footer class="footer">
-<?php get_template_part( 'nav', 'below-single' ); ?>
-</footer>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  
+    <?php get_template_part( 'entry' ); ?>
+  
+    <?php if ( comments_open() && ! post_password_required() ) { comments_template( '', true ); } ?>
+    <?php endwhile; endif; ?>
+
+    <footer class="footer">
+    <?php get_template_part( 'nav', 'below-single' ); ?>
+    </footer>
 </main>
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
