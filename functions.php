34a3496b50343eccc9fb0662d8a2027a8182472b
jvanstone <jason@vanstoneonline.com>
2021-02-19 15:19:01 -0500

    add main login

--- functions.php
+++ functions.php
@@ -255,6 +255,38 @@ function canadian_guide_scripts() {
 add_action( 'wp_enqueue_scripts', 'canadian_guide_scripts' );
 
 
+/*******
+ * 
+ *  Change the login screen to show a custom logo
+ * 
+ * 
+ * 
+ */
+
+function canadian_guide_logo() { ?>
+	<style type=”text/css”>
+	#login h1 a, .login h1 a {
+	background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/uploads/2021/02/cropped-canadaleaf-background-768x1074-1.png);
+	height:65px;
+	width:320px;
+	background-size: 320px 65px;
+	background-repeat: no-repeat;
+	padding-bottom: 30px;
+	}
+	</style>
+<?php }
+	add_action( ‘login_enqueue_scripts’, ‘canadian_guide_logo’ );
+
+	function my_login_logo_url() {
+		return home_url();
+	}
+	add_filter( 'login_headerurl', 'my_login_logo_url' );
+	
+	function my_login_logo_url_title() {
+		return 'Your Site Name and Info';
+	}
+	add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
     /**
      * 
      *  A Filter to make Bootstrap Drop-down work better with WordPress
