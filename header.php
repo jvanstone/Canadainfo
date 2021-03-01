0efdac74c84b57656c41c0eab2f0053369aa3472
jvanstone <jason@vanstoneonline.com>
2021-03-01 13:08:28 -0500

    header update

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
     
     <head>
         <meta charset="<?php bloginfo( 'charset' , 'canadian_guide' ); ?>" />
-        <meta name="viewport" content="width=device-width" />
+        <meta name="viewport" content="width=device-width, initial=scale=1" />
         <?php wp_head(); ?>
     </head>
 
