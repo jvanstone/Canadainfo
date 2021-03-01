5110471b88945aafe6869421f6d450bbab254ad8
jvanstone <jason@vanstoneonline.com>
2021-03-01 14:10:01 -0500

    add up icon

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
     <footer id="colophon" class="site-footer no-print" role="contentinfo">
-    <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+    <button onclick="topFunction()" id="myBtn" title="Go to top"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-up" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z"/>
+</svg><br>Top</button>
     <?php if ( has_nav_menu( 'footer' ) ) : ?>
         <nav aria-label="<?php esc_attr_e( 'Secondary menu', 'canadian_guide' ); ?>" class="footer-navigation">
             <ul class="footer-navigation-wrapper">
