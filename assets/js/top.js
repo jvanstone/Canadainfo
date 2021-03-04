34b3eb2fac448b784a9c1b890c7d89dbde5560bd
jvanstone <jason@vanstoneonline.com>
2021-03-04 14:25:37 -0500

    update to to Top js

--- assets/js/top.js
+++ assets/js/top.js
@@ -1,14 +1,14 @@
 //Get the button
-var mybutton = document.getElementById("goTop");
+var gotop = document.getElementById("goTop");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    gotop.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    gotop.style.display = "none";
   }
 }
 
