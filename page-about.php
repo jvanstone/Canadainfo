2d90254e4c5f21e7523d278cb956d64c17e1c172
jvanstone <jason@vanstoneonline.com>
2021-03-05 09:42:25 -0500

    update to about

--- page-about.php
+++ page-about.php
@@ -3,7 +3,8 @@
  * Template Name: About Page
  * Description: Takes the Post Category { About } and displays the content on this page 
  * Created by: Vanstone Online - Jason Vanstone
- *
+ *  
+ * Removed links to single post  - >  the_permalink()
  * 
  */
 ?>
@@ -20,21 +21,22 @@
     <?php
     $args = array( 
         'posts_per_page' => 3, 
-        'offset'=> 0, 
-        'category' => 8,
-        'orderby'         => 'date',
-        'order'            => 'ASC', );
+        'offset'         => 0, 
+        'category'       => 8,
+        'orderby'        => 'date',
+        'order'          => 'ASC', 
+    );
 
     $myposts = get_posts( $args );
     foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
         <div class="card mb-3" style="max-width: 500px; text-align: center; margin:0 auto;">
             <div class="row g-0">
                 <div class="col-12 col-md-4">
-                <a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail( $page->ID, 'thumbnail' ); ?></a>
+                <?php echo get_the_post_thumbnail( $page->ID, 'thumbnail' ); ?>
                 </div>
                 <div class="col-12 col-md-8">
                     <div class="card-body" style="text-align: center;">
-                        <h5 class="card-title" style="text-align: center;"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                        <h5 class="card-title" style="text-align: center;"><?php the_title(); ?></h5>
                         <?php $content = apply_filters( 'the_content', get_the_content() ); ?>
                         <div class="card-text" style="text-align: center;"><?php echo $content;?></div>
                     </div> 
@@ -51,6 +53,7 @@
     <footer class="footer">
     <?php get_template_part( 'nav', 'below-single' ); ?>
     </footer>
+    
 </main>
 
 <?php get_sidebar(); ?>
