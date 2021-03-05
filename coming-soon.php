b6066a56acff9c0d8552950b7a22db9596bc1183
jvanstone <jason@vanstoneonline.com>
2021-03-05 09:28:23 -0500

    update to coming soon

--- coming-soon.php
+++ coming-soon.php
@@ -35,23 +35,15 @@
     </header>
 
     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header>
-    <?php if ( is_singular() ) {
-    echo '<h1 class="" >';
-    } else {
-    echo '<h2 class="">';
-    } ?>
-    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
-    <?php if ( is_singular() ) {
-    echo '</h1>';
-    } else {
-    echo '</h2>';
-    } ?> 
-    <?php if ( ! is_search() ) { get_template_part( 'entry', 'meta' ); } ?>
+     <header>
+        <h1 class="coming-soon">
+            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+        </h1>
     </header>
-    <?php get_template_part( 'entry', ( is_front_page() || is_home() || is_front_page() && is_home() || is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
-    <?php if ( is_singular() ) { get_template_part( 'entry-footer' ); } ?>
-</article>
+
+    <?php //get_template_part( 'entry'); ?>
+   
+    </article>
    
     </div> <!-- End Wrapper -->
       
