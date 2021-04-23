1e30f81f5511e8a57e70a2a66f0786ddaa2b0912
Jason Vanstone <jason@vanstoneonline.com>
2021-04-22 20:07:27 -0400

    rename theme

--- functions.php
+++ functions.php
@@ -765,3 +765,11 @@ add_action( 'wp_footer', 'canada_info_add_ie_class' );
 
 
 include ('inc/pmpro-changes.php');
+
+
+/* function wp_maintenance_mode() {
+	if (!current_user_can('edit_themes') || !is_user_logged_in()) {
+	wp_die('<h1>Canadainfo is under maintenance</h1><br />Our website is under some planned maintenance. Please check back later.');
+	}
+	}
+	add_action('get_header', 'wp_maintenance_mode'); */
