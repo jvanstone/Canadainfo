f784c786572c4c4742d04b5925847a96a2bb70c0
Jason Vanstone <jason@vanstoneonline.com>
2021-04-23 10:26:51 -0400

    maintenance mode

--- functions.php
+++ functions.php
@@ -767,9 +767,9 @@ add_action( 'wp_footer', 'canada_info_add_ie_class' );
 include ('pmpro-custom/pmpro-changes.php');
 
 
-/* function wp_maintenance_mode() {
+function wp_maintenance_mode() {
 	if (!current_user_can('edit_themes') || !is_user_logged_in()) {
 	wp_die('<h1>Canadainfo is under maintenance</h1><br />Our website is under some planned maintenance. Please check back later.');
 	}
 	}
-	add_action('get_header', 'wp_maintenance_mode'); */
+	add_action('get_header', 'wp_maintenance_mode'); 
