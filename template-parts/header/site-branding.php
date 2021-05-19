6f836656bb4f8c65019b4323b6feb95a71e810c3
Jason Vanstone <jason@vanstoneonline.com>
2021-05-19 11:41:42 -0400

    remove no-print

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -4,7 +4,6 @@
  *
  * @category   WordPress_Theme
  * @package    Canadainfo
- * 
  * @author     Vanstone Online <jason@vanstoneonline.com>
  * @license    GPL 3.0 http://www.gnu.org/licenses/gpl-3.0.html
  * @link       https://developer.wordpress.org/themes/basics/template-hierarchy/
--- template-parts/header/site-nav.php
+++ template-parts/header/site-nav.php
@@ -13,7 +13,7 @@
 		<img src="<?php header_image(); ?>" class="img-fluid header-image" alt="<?php echo get_bloginfo( 'name', 'canada-info' ); ?>" />
 
 		<!-- Top Navigation Menu -->
-		<nav class="navbar navbar-expand-md navbar-dark bg-dark no-print">
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark">
 			<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"></a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
