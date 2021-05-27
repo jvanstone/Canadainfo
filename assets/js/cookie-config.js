1cc974cd59b260c151d2d23599b0c64278075326
Jason Vanstone <jason@vanstoneonline.com>
2021-05-27 14:05:01 -0400

    update to cookie sizing

--- assets/js/cookie-config.js
+++ assets/js/cookie-config.js
@@ -10,7 +10,8 @@ var options = {
 		console.log( myPreferences );
 	},
 	uncheckBoxes: true,
-	acceptBtnLabel: 'Accept Cookies',
+	acceptBtnLabel: 'Accept',
+	customBtnLabel: 'Customize',
 	moreInfoLabel: 'More information',
 	cookieTypesTitle: 'Select which cookies you want to accept',
 	fixedCookieTypeLabel: 'Essential',
